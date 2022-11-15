5f719977459ddbddbd43e871877cb83ce91f6d9a
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-11-15 17:02:55 -0500

    Renamed data logger

--- Subscale/acs.py
+++ Subscale/acs.py
@@ -4,20 +4,20 @@ if FAKE_DATA:
     import sensors_spoof as sensors
 else:
     import sensors
-import datalogger
+import data_logger
 
-datalogger.find_new_filename(datalogger.path, datalogger.name, datalogger.extension)
-datalogger.new_CSV(datalogger.filename, datalogger.header)
+data_logger.find_new_filename(data_logger.path, data_logger.name, data_logger.extension)
+data_logger.new_CSV(data_logger.filename, data_logger.header)
 sensors.init_sensors()
 samples = 0
 
 while True:
     sensors.read_sensors()
-    datalogger.addRow(datalogger.f)
+    data_logger.addRow(datalogger.f)
     samples += 1
     print("--------------------------------------------------")
     print(f"\nSample #{samples}; Time: {sensors.curr_time}s")
     print(f"Sample Rate = {samples/sensors.curr_time}Hz\n")
     print("--------------------------------------------------")
 
-datalogger.f.close()
+data_logger.f.close()
